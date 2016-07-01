2f3d0bce2668167aff4967e486a3e624a0d2258a
Kazi Munshimun <anik1.bd@gmail.com>
2016-07-01 17:08:46 +0600

    mrealstate initial project commit

---
+++ iOS/mRealState/mRealState/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mRealState
+//
+//  Created by Anik on 7/1/16.
+//  Copyright © 2016 mStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/mRealState/mRealState/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mRealState
+//
+//  Created by Anik on 7/1/16.
+//  Copyright © 2016 mStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
