addbc405572807a7574148bca5ddaa081068532a
Kazi Munshimun <anik1.bd@gmail.com>
2016-07-17 00:24:52 +0600

    Firebase sdk added

--- iOS/mRealState/mRealState/Controller/RSPropertyDetailViewController.m
+++ iOS/mRealState/mRealState/Controller/RSPropertyDetailViewController.m
@@ -13,6 +13,8 @@
 -(void)viewDidLoad{
     [super viewDidLoad];
     
+    self.title = @"Detail";
+    
     OverviewViewController *overviewViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"overviewViewController"];
     overviewViewController.title = @"Overview";
     
@@ -27,9 +29,9 @@
     
     NSArray *controllerArray = @[overviewViewController, featureTableViewController, locationViewController, reviewTableViewController];
     NSDictionary *parameters = @{
-                                 CAPSPageMenuOptionScrollMenuBackgroundColor: [UIColor colorWithRed:30.0/255.0 green:30.0/255.0 blue:30.0/255.0 alpha:1.0],
+                                 CAPSPageMenuOptionScrollMenuBackgroundColor: [UIColor colorWithRed:36.0/255 green:105.0/255 blue:69.0/255 alpha:1.0],
                                  CAPSPageMenuOptionViewBackgroundColor: [UIColor colorWithRed:20.0/255.0 green:20.0/255.0 blue:20.0/255.0 alpha:1.0],
-                                 CAPSPageMenuOptionSelectionIndicatorColor: [UIColor colorWithRed:36.0/255 green:105.0/255 blue:69.0/255 alpha:1.0],
+                                 CAPSPageMenuOptionSelectionIndicatorColor: [UIColor colorWithRed:59.0/255 green:165.0/255 blue:203.0/255 alpha:1.0],
                                  CAPSPageMenuOptionBottomMenuHairlineColor: [UIColor colorWithRed:70.0/255.0 green:70.0/255.0 blue:70.0/255.0 alpha:1.0],
                                  CAPSPageMenuOptionMenuItemFont: [UIFont fontWithName:@"HelveticaNeue" size:13.0],
                                  CAPSPageMenuOptionMenuHeight: @(40.0),
